7703ac18a0733de4d98dd0cdf619062ceb8530b1
s-nshd <s.nshd@outlook.jp>
2023-01-23 21:56:11 +0900

    containerのスタイルをクラスにまとめたのと著者と共有をcol-6にまとめた

--- pages/blog/article/_slug.vue
+++ pages/blog/article/_slug.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="container">
+  <main class="container p-lg-5 p-2">
     <article v-if="currentArticle">
       <img :src="currentArticle.coverImage.src" alt="" class="rounded-3"/>
       <h2 class="mb-3">{{ currentArticle.title }}</h2>
@@ -9,7 +9,8 @@
         </li>
       </ul>
       <div class="row mb-5 d-flex align-items-center">
-        <a href="#" class="col-2">
+        <div class="col-6">
+          <a href="#">
           <template
             v-if="
               currentArticle.author && currentArticle.author.profileImage
@@ -38,7 +39,6 @@
             </svg>
           </template>
         </a>
-        <div class="col-6">
           <NuxtLink
             :to="`/blog/author/${currentArticle.author.slug}`"
             class="Article_AuthorName text-dark text-decoration-none text-reft"
@@ -48,36 +48,34 @@
             publishDate
           }}</time>
         </div>
-        <div class="col-4">
-          <div class="row d-flex align-items-center">
-            <div class="text-muted">この記事を共有</div>
-            <button type="button" @click="shareOnTwitter" class="btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#cccccc"
-              >
-                <path
-                  d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z"
-                />
-              </svg>
-            </button>
-            <button type="button" @click="shareOnFacebook" class="btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#cccccc"
-              >
-                <path
-                  d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"
-                />
-              </svg>
-            </button>
-          </div>
+        <div class="col-6">
+          <div class="text-muted">この記事を共有</div>
+          <button type="button" @click="shareOnTwitter" class="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#cccccc"
+            >
+              <path
+                d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z"
+              />
+            </svg>
+          </button>
+          <button type="button" @click="shareOnFacebook" class="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#cccccc"
+            >
+              <path
+                d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
       <!-- eslint-disable-next-line vue/no-v-html -->
@@ -359,9 +357,6 @@ export default {
 }
 </script>
 <style scoped>
-.container {
-  padding: 3.5vw;
-}
 img {
   width: 84.8vw;
   height: 36vw;
