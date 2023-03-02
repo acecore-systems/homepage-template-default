e54de91d476a1b918f4781d57ca0c5fe61f1fc44
s-nshd <s.nshd@outlook.jp>
2023-03-02 11:25:36 +0900

    styleをbootstrapに直した

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="Pagination">
+  <nav class="Pagination p-5">
     <ul class="Pagination_Items px-0 list-unstyled fs-0.5 bg-secondary border rounded d-inline-block position-absolute start-50">
       <li v-for="page in pages" :key="page.number" class="Pagination_Item">
         <NuxtLink type="button" :to="`${basePath}/page/${page.number}`" :class="`Pagination_Button text-decoration-none btn btn-secondary ${page.isCurrent ? '_current' : ''}`">{{page.number}}</NuxtLink>
@@ -42,7 +42,3 @@ export default {
 </script>
 
 <style scoped>
-.Pagination{
-  padding: 20%;
-}
-</style>
