49c7a64fe30d5a7d082558143fef6bf997ff083e
s-nshd <s.nshd@outlook.jp>
2023-03-09 12:19:01 +0900

    指摘頂いた所を消した

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,11 +1,13 @@
 <template>
-  <nav class="Pagination p-5">
+  <nav class="p-5">
     <ul
-      class="Pagination_Items px-0 list-unstyled fs-0.5 bg-secondary border rounded d-inline-block position-absolute start-50">
+      class="px-0 list-unstyled fs-0.5 bg-secondary border rounded d-inline-block position-absolute start-50">
       <li v-for="page in pages" :key="page.number" class="Pagination_Item">
-        <NuxtLink type="button" :to="`${basePath}/page/${page.number}`"
-          :class="`Pagination_Button text-decoration-none btn btn-secondary ${page.isCurrent ? '_current' : ''}`">
-          {{ page.number }}</NuxtLink>
+        <NuxtLink
+          type="button" 
+          :to="`${basePath}/page/${page.number}`"
+          :class="`text-decoration-none btn btn-secondary ${page.isCurrent ? '_current' : ''}`"
+        >{{ page.number }}</NuxtLink>
       </li>
     </ul>
   </nav>
