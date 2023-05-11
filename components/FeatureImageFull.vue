80781e8af2ae5a8e3796f2a60ed33bf74df0cce0
s-nshd <s.nshd@outlook.jp>
2023-05-11 11:57:36 +0900

    textを中央寄せにした

--- components/FeatureImageFull.vue
+++ components/FeatureImageFull.vue
@@ -2,10 +2,10 @@
   <section class="Feature1">
     <h3
       v-if="data.titleCopy"
-      class="Feature1_Title"
+      class="Feature1_Title text-center"
       v-html="data.titleCopy"
     ></h3>
-    <p v-if="data.text" class="Feature1_Text" v-html="data.text"></p>
+    <p v-if="data.text" class="Feature1_Text text-center" v-html="data.text"></p>
     <img
       v-if="data.image && data.image.src"
       :src="data.image.src"
