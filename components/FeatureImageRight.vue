dc449323cf326f92fee68fbf841510b90eca6a19
s-nshd <s.nshd@outlook.jp>
2023-05-31 18:04:02 +0900

    フォーマットした

--- components/FeatureImageRight.vue
+++ components/FeatureImageRight.vue
@@ -1,18 +1,11 @@
 <template>
-  <section class="my-5">
-    <div class="row">
-    <div class="d-flex bd-highlight">
-      <div class="col-6 bd-highlight">
-        <h3
-          v-if="data.titleCopy"
-          v-html="data.titleCopy"
-        ></h3>
-        <p v-if="data.text" v-html="data.text"></p>
-      </div>   
-      <div v-if="data.image && data.image.src" class="col-6 bd-highlight">
-        <img :src="data.image.src" alt="" class="img-fluid w-100"/>
-      </div>
+  <section class="row mx-lg-5 mx-3 my-5">
+    <div class="col-6">
+      <h3 v-if="data.titleCopy">{{ data.titleCopy }}</h3>
+      <p v-if="data.text">{{ data.text }}</p>
     </div>
+    <div v-if="data.image && data.image.src" class="col-6">
+      <img :src="data.image.src" alt="" class="img-fluid w-100" />
     </div>
   </section>
 </template>
