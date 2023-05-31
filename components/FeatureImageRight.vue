634c4dfb9ca3cf1f0bfef527e9c4bb6463804ddf
s-nshd <s.nshd@outlook.jp>
2023-05-31 22:04:37 +0900

    画面に合わせて改行するようにした

--- components/FeatureImageRight.vue
+++ components/FeatureImageRight.vue
@@ -4,7 +4,7 @@
       <h3 v-if="data.titleCopy">{{ data.titleCopy }}</h3>
       <p v-if="data.text">{{ data.text }}</p>
     </div>
-    <div v-if="data.image && data.image.src" class="col-6">
+    <div v-if="data.image && data.image.src" class="col-lg-6">
       <img :src="data.image.src" alt="" class="img-fluid w-100" />
     </div>
   </section>
