ebfa5b46156c548784ca12552606f06bb0f7387f
s-nshd <s.nshd@outlook.jp>
2023-05-31 21:44:02 +0900

    marginを追加してフォーマットして不要な箇所を消した

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,30 +1,17 @@
 <template>
-  <section class="container mb-5">
-    <div class="row">
+  <section class="container my-5 ">
+    <div class="row mx-lg-5 mx-3">
       <div class="col-6 d-flex align-items-center p-4">
         <div>
-          <p
-          v-if="data.shoulderCopy"
-          class="text-muted"
-          v-html="data.shoulderCopy"
-        ></p>
-        <h2
-          v-if="data.titleCopy"
-          v-html="data.titleCopy"
-        ></h2>
-        <p v-if="data.text" v-html="data.text"></p>
-        <a
-          v-if="data.ctaButton && data.ctaButton.label"
-          class="btn btn-outline-primary"
-          :href="data.ctaButton.url"
-          rel="noreferrer noopener"
-          :target="data.ctaButton.newTab ? '_blank' : ''"
-          >{{ data.ctaButton.label }}</a
-        >
+          <p v-if="data.shoulderCopy" class="text-muted">{{ data.shoulderCopy }}</p>
+          <h2 v-if="data.titleCopy">{{ data.titleCopy }}</h2>
+          <p v-if="data.text">{{ data.text }}</p>
+          <a v-if="data.ctaButton && data.ctaButton.label" class="btn btn-outline-primary" :href="data.ctaButton.url"
+            rel="noreferrer noopener" :target="data.ctaButton.newTab ? '_blank' : ''">{{ data.ctaButton.label }}</a>
         </div>
       </div>
-      <div v-if="data.backgroundImage" class="col-lg-6">
-        <img :src="data.backgroundImage.src" class="img-fluid rounded-4 w-100 h-auto my-lg-3 mb-3" alt="" />
+      <div v-if="data.backgroundImage" class="col-lg-6 p-0">
+        <img :src="data.backgroundImage.src" class="img-fluid rounded-4 w-100 h-auto" alt="" />
       </div>
     </div>
   </section>
