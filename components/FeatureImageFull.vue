705bfc99f2ac66072d73fe3dbb9110adda807a87
s-nshd <s.nshd@outlook.jp>
2023-05-31 22:00:55 +0900

    marginを追加してフォーマットした

--- components/FeatureImageFull.vue
+++ components/FeatureImageFull.vue
@@ -1,16 +1,15 @@
 <template>
-  <section class="my-5">
+  <section class="my-5 mx-lg-5 mx-3">
     <h3
       v-if="data.titleCopy"
-      class="text-center"
-      v-html="data.titleCopy"
-    ></h3>
-    <p v-if="data.text" class="text-center mb-4" v-html="data.text"></p>
+      class="text-center mx-3"
+    >{{data.titleCopy}}</h3>
+    <p v-if="data.text" class="text-center mb-4 mx-3">{{data.text}}</p>
     <img
       v-if="data.image && data.image.src"
       :src="data.image.src"
       alt=""
-      class="d-block object-fit-cover px-lg-5 px-3 w-100"
+      class="d-block object-fit-cover w-100 p-3"
       style="height: 25vw;"
     />
   </section>
