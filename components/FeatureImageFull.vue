c567c2356a5fbca70b0bf48a11c6e564d67b5ca7
s-nshd <s.nshd@outlook.jp>
2023-06-01 10:24:27 +0900

    レビューで指摘頂いた点の修正を改行した

--- components/FeatureImageFull.vue
+++ components/FeatureImageFull.vue
@@ -2,7 +2,8 @@
   <section class="my-5 mx-lg-5 mx-3">
     <h3 v-if="data.titleCopy" class="text-center mx-3">{{ data.titleCopy }}</h3>
     <p v-if="data.text" class="text-center mb-4 mx-3">{{ data.text }}</p>
-    <img v-if="data.image && data.image.src" :src="data.image.src" alt="" class="d-block object-fit-cover w-100 p-3"
+    <img v-if="data.image && data.image.src" :src="data.image.src" alt="" 
+      class="d-block object-fit-cover w-100 p-3"
       style="height: 25vw;" />
   </section>
 </template>
